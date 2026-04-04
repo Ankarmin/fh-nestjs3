fb9234ae4e16c16a24eba892e7de21f9f1de0cd2
Ankarmin <davidspr.22@gmail.com>
2026-04-03 23:26:45 -0500

    Sección 8 Avance 2

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { SeedService } from './seed.service';
 
 @Module({
   controllers: [SeedController],
